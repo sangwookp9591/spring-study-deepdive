49051cb1dff033e187f01ef43bb8c0f8a73e864e
sangwookpark <sangwookp9591@gmail.com>
2025-09-24 16:29:06 +0900

    feat : 기본  orderService 추가

---
+++ src/main/java/hello/advanced/v0/OrderService.java
@@ -0,0 +1,19 @@
+package hello.advanced.v0;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor // 생성자를 만들어줌
+public class OrderService {
+
+    // 하나만있으면 자동으로 @Autowired를 적용해줌
+    private final OrderRepositoryV0 orderRepository;
+
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+
+}
